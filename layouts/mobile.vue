2681441bee60d1a4385e13caedb8a0184766ece8
Olive Casazza <olive.casazza@schrodinger.com>
2025-04-12 14:46:40 -0700

    working on layouts

---
+++ layouts/mobile.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="custom">
+    <slot name="header">
+      <p>
+        <i>Default #header slot content for <code>layouts/custom.vue</code></i>
+      </p>
+    </slot>
+    <slot />
+  </div>
+</template>
